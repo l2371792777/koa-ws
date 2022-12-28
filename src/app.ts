461731f50b364a985918bcad58d79baea6246098
ayanami <l2371792777@163.com>
2022-12-28 11:34:06 +0800

    new code

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@
  * @author 川
  */
 
-// const Koa=require('koa');
+
 import Koa from 'koa';
+import Router from 'koa-router';
 
 let app=new Koa();
+let router=new Router();
+
+app.use(router.routes());
 
 app.listen(3030);
 console.log("OK");
